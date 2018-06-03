7f998c07816c0f9ac86df6bf3e12234f3b32796a
BlueReptile <callmekalango@gmail.com>
2018-06-03 15:36:51 -0300

    define custom error/404 page

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -10,6 +10,8 @@ module Foundation where
 import Import.NoFoundation
 import Database.Persist.Sql (ConnectionPool, runSqlPool)
 import Yesod.Core.Types     (Logger)
+import Text.Lucius
+import Text.Julius
 
 data App = App
     { appSettings    :: AppSettings
@@ -23,6 +25,24 @@ mkYesodData "App" $(parseRoutesFile "config/routes")
 
 instance Yesod App where
     makeLogger = return . appLogger
+    errorHandler NotFound = do
+      maybeId <- lookupSession "ID"
+      idText <- case maybeId of
+          (Just id) -> do
+              return id
+          _ -> do
+              return ""
+      fmap toTypedContent $ defaultLayout $ do
+        addStylesheet $ (StaticR css_materialize_css)
+        addScript $ (StaticR js_jquery_js)
+        addScript $ (StaticR js_materialize_js)
+        toWidget $(juliusFile "templates/admin.julius")
+        toWidget $(luciusFile "templates/admin.lucius")
+        $(widgetFile "header")
+        $(widgetFile "error/404")
+        $(widgetFile "footer")
+    errorHandler other = defaultErrorHandler other
+
 
 type Form a =
         Html -> MForm Handler (FormResult a, Widget)
